12b68b2c06e478ed37a00fc33f0410638a535bef
eboyle3 <156923051+eboyle3@users.noreply.github.com>
2024-01-26 22:29:49 +0000

    Add files via upload
    Returns the distance from the robot to a nearby object eg tennis ball

---
+++ HC-SR04_test_code.ino
@@ -0,0 +1,32 @@
+#define triggerPin 10
+#define echoPin 11
+float distance;
+int msTimeTaken;
+
+void setup() {
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(triggerPin, LOW);
+  
+  delayMicroseconds(2);
+  //pulse trigger pin for 10 micros
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  //start clock at 0 to 
+  msTimeTaken = 0;
+
+  do{
+    msTimeTaken = msTimeTaken + 1;
+    delayMicroseconds(1);
+    
+  }
+  while(echoPin != HIGH);
+  
+  distance = (msTimeTaken/2000000) * 343;
+  Serial.println(distance * 100, 2);
+}
