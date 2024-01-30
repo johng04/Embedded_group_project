e2c4e5acf5d281e71d56fee1478bcacf4cd6d5cb
eboyle3 <156923051+eboyle3@users.noreply.github.com>
2024-01-30 22:38:33 +0000

    Add files via upload

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -2,24 +2,20 @@
 #define echoPin 11
 float distance;
 float duration;
-String input;
-float* values = new float[10];
-
+int STATE = 1;
 void setup() {
   pinMode(triggerPin, OUTPUT);
+  pinMode(4, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Press enter to go !!!!!!");
-  
 
-    while(Serial.available() == 0);{
+    while(STATE == 1){
   
       for(int i = 0; i < 10; i ++){
-        digitalWrite(triggerPin, LOW);
-      
+        digitalWrite(triggerPin, LOW); 
         delayMicroseconds(2);
         //pulse trigger pin for 10 micros
         digitalWrite(triggerPin, HIGH);
@@ -27,24 +23,14 @@ void loop() {
         digitalWrite(triggerPin, LOW);
         //start clock at 0 to 
         duration = pulseIn(echoPin, HIGH);
-        distance = (duration*.0343)/2;
-
+        distance = (duration * 0.0343)/2;
+        if(distance < 8.00){
+          digitalWrite(4, HIGH);
+        }
+        else{digitalWrite(4,LOW);}
         Serial.println(distance);
-        values[i] = distance;
-        delay(1000);
+        delay(200);
+
       }
     }
-
-//MEASURING TAPE FROM JACK
-  float sum;
-  for(int i = 0; i < 10; i ++){
-    sum = sum + values[i];
-  }
-
-  float mean = sum / 10;
-  Serial.println("__________________________"); 
-  Serial.println(mean);
- 
-  delay(10000);
- 
 }
