b9ab72e8f392c08105513fef597599f27f483be8
john ciaran goyvaerts <143227446+johng04@users.noreply.github.com>
2024-01-30 15:34:49 +0000

    Update Motor_Tests.ino
    motor speed control by pentodomotor attaced 2 wheels per maker board

--- Motor_Tests.ino
+++ Motor_Tests.ino
@@ -1,19 +1,14 @@
-#define m_one_a 10 //front left
-#define m_one_b 11 //front right
-#define m_two_a 12 //back left
-#define m_two_b 13 //back right
+ #define VOLTAGE_INPUT A0 
+#define front 11
+#define back 10
 
 void setup() {
-  analogWrite(m_one_a, 100);
-  analogWrite(m_one_b, 100);
-  analogWrite(m_two_a, 100);
-  analogWrite(m_two_b, 100);
+
 }
 
 void loop() {
-  delay(5000);
-  analogWrite(m_one_a, 0);
-  analogWrite(m_one_b, 0);
-  analogWrite(m_two_a, 0);
-  analogWrite(m_two_b, 0);
+int rawValue = analogRead(VOLTAGE_INPUT);
+int output_val=map(rawValue,0,1023, 0,100);
+  analogWrite(front, output_val);
+  analogWrite(back, output_val);
 }
